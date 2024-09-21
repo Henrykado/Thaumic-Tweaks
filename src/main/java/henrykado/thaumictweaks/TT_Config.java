7ca7a18817198b19b784431d7070c3a8c568284f
Henrykado <henrique.pitasse.cunha4@gmail.com>
2024-09-21 20:47:35 -0300

    oopsie

--- src/main/java/henrykado/thaumictweaks/TT_Config.java
+++ src/main/java/henrykado/thaumictweaks/TT_Config.java
@@ -8,7 +8,6 @@ import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import zone.rong.mixinbooter.IEarlyMixinLoader;
 
 import java.util.List;
 import java.util.Map;
@@ -79,41 +78,6 @@ public class TT_Config {
 		if (event.getModID().equals(ThaumicTweaks.MODID))
 		{
 			ConfigManager.sync(ThaumicTweaks.MODID, Config.Type.INSTANCE);
-		}
-	}
-
-	@IFMLLoadingPlugin.MCVersion("1.12.2")
-	@IFMLLoadingPlugin.TransformerExclusions({"henrykado.thaumictweaks", "henrykado.thaumictweaks.asm"})
-	public static class TT_Plugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
-
-		@Override
-		public String[] getASMTransformerClass() {
-			return null; //new String[] {"henrykado.thaumictweaks.asm.TT_ClassTransformer"};
-		}
-
-		@Override
-		public String getModContainerClass() {
-			return null;
-		}
-
-		@Override
-		public String getSetupClass() {
-			return null;
-		}
-
-		@Override
-		public void injectData(Map<String, Object> data) {
-
-		}
-
-		@Override
-		public String getAccessTransformerClass() {
-			return null;
-		}
-
-		@Override
-		public List<String> getMixinConfigs() {
-			return ImmutableList.of("mixins.thaumictweaks.json");
 		}
 	}
 }
--- src/main/java/henrykado/thaumictweaks/mixin/MixinProxyBlock.java
+++ src/main/java/henrykado/thaumictweaks/mixin/MixinProxyBlock.java
@@ -1,15 +1,12 @@
 package henrykado.thaumictweaks.mixin;
 
 import henrykado.thaumictweaks.TT_Config;
-import henrykado.thaumictweaks.ThaumicTweaks;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.statemap.IStateMapper;
 import net.minecraftforge.client.model.ModelLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import thaumcraft.common.blocks.world.ore.ShardType;
 import thaumcraft.proxies.ProxyBlock;
 
 @Mixin(ProxyBlock.class)
